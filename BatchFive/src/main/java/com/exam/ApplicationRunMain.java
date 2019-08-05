8989615a2ef36a38eed70d9acd405cd2eb877ce1
Shaariful Islam <naztechbatch5@gmail.com>
2019-08-05 15:09:32 +0600

    Checking New Git

--- BatchFive/src/main/java/com/exam/ApplicationRunMain.java
+++ BatchFive/src/main/java/com/exam/ApplicationRunMain.java
@@ -2,7 +2,8 @@ package com.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// Date today: 8/5/2019
+// @Author : Md. Shariful Islam
+// Time: 8/5/2019 , 3:09 PM
 @SpringBootApplication
 public class ApplicationRunMain {
 public static void main(String[] args) {
